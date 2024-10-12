c0c65029ae7d9a96e43bc50f532e52b0ce185749
HinJaban <nikita.bek22@gmail.com>
2024-10-12 22:23:57 +0300

    123.4

--- Birn.cpp
+++ Birn.cpp
@@ -3,7 +3,8 @@
 struct date
 {
     int month, day, year;
-    void set(int,int,int);
+    /*функции-члены структуры; их можно вызвать только через переменные соответствующего типа...*/
+    void set(int,int,int); 
     void get(int*,int*,int*);
     void next();
     void print();
@@ -12,12 +13,18 @@ date today;
 date my_birthday;
 void f()
 {
-    my_birthday.set(30,12,1950);
+    my_birthday.set(30,12,1950);//...используя стандартную запись обращения к члену структуры через точку
     today.set(18,1,1991);
     my_birthday.print();
     today.next();
 }
-
+void date::next() //так как разные структуры могут иметь функции-члены с одинаковыми именами, при определении функции-члена нужно указывать имя структуры
+{
+    if(++day>28)
+    {
+        //здесь сложный вариант
+    }
+}
 int main()
 {
     std::cout<<"Hello"<<std::endl;
