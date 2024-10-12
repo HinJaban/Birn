b719e119fc0d4f711d89e019086ae5fd373b8ac2
HinJaban <nikita.bek22@gmail.com>
2024-10-12 22:16:55 +0300

    123.3

--- Birn.cpp
+++ Birn.cpp
@@ -9,6 +9,14 @@ struct date
     void print();
 };
 date today;
+date my_birthday;
+void f()
+{
+    my_birthday.set(30,12,1950);
+    today.set(18,1,1991);
+    my_birthday.print();
+    today.next();
+}
 
 int main()
 {
