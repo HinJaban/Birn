34b9f7282834c5f0b6d3f3898cdedcc891f387db
HinJaban <nikita.bek22@gmail.com>
2024-10-12 22:05:02 +0300

    123

--- Birn.cpp
+++ Birn.cpp
@@ -1,4 +1,13 @@
 #include <iostream>
+
+struct date
+{
+    int month, day, year;
+};
+date today;
+void set_date(date*,int,int,int);
+void next_date(date*);
+void print_date(const date*);
 int main()
 {
     std::cout<<"Hello"<<std::endl;
